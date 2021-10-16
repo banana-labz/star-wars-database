848823a63471bb58d79e35341c04aa304b3df36a
THXRION666 <strike201420141@gmail.com>
2021-10-16 12:36:53 +0300

    changed header style

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,9 +1,13 @@
 import "./header.css"
 
 export const Header = () => (
-    <div className="header d-flex">
-        <h3><a href="">Star db</a></h3>
-        <ul className="d-flex">
+    <div className="header">
+        <h3 style={{marginBottom: 0}}>
+            <a href="" className="header-main-text">
+                Star db
+            </a>
+        </h3>
+        <ul style={{marginBottom: 0}}>
             <li>
                 <a href="#">People</a>
             </li>
