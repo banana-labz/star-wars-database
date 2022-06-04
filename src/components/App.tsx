c6d6dd18da6f848515fe5c3789531e95394df463
therion-666 <ilya.martsenuk@meta.ua>
2022-06-04 20:36:04 +0300

    adding random planet

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -8,10 +8,12 @@ import {
 } from "react-router-dom"
 
 import Header from "components/Header"
+import RandomPlanet from "components/RandomPlanet"
 
 const App = () => (
   <BrowserRouter>
     <Header />
+    <RandomPlanet />
     <Routes>
       <Route path="/" element={<Navigate to="/home"/>} />
       <Route path="home" element={<>home</>} />
---
+++ src/components/RandomPlanet.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+// 2B2B2B
+
+const RandomPlanet = () => (
+  <div>
+    random-planet
+  </div>
+)
+
+export default RandomPlanet
